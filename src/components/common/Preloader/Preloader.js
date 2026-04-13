0c37dd356d733e9a9efffc965be84bcb19656bb3
VasKaleev <vkaleev.fam@gmail.com>
2026-04-13 13:35:53 +0300

    Updates

--- src/components/common/Preloader/Preloader.js
+++
@@ -1,9 +0,0 @@
-import preloader from '../../../assets/images/preloader.svg';
-let Preloader = (props) => {
-  return (
-    <div>
-      <img src={preloader} alt="" style={{ backgroundColor: 'white' }} />
-    </div>
-  );
-};
-export default Preloader;
